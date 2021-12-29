72dddaaeefb6d8a14ee343967a4ef44b640e56c1
MatthewHautala <november12erika@gmail.com>
2021-12-29 15:34:52 -0700

    09

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -3,21 +3,42 @@ Write a function named: interrupter(interruptingWord). The interrupter function 
 accept a word and return a function. When the function returned by interrupter
 is invoked with a string the string will be returned with "interruptions".
 
-Look below to see how this function is invoked:
+
+
+***********************************************************************/
+function interrupter (interruptingWord) {
+  return function (sentence) {
+    let words = sentence.split(' ');// split sentence into an array of words
+    let newStr = ""//make a new string to add the  interrupters
+
+   for (let i = 0; i < words.length; i++){// iterate through words array
+
+    let word = words[i];
+    //console.log(word, '---')
+
+    if (i === words.length - 1) {
+      newStr += word
+    }
+     else {
+      newStr += word + ' ' + interruptingWord + " "
+      //first iteration newStr = how what // are what // you
+    }
+
+   }
+   return newStr
+  }
+
+}
+
+////Look below to see how this function is invoked:
 let rudePerson = interrupter("what"); // => returns a function
 console.log(rudePerson("how are you")); // prints "how what are what you"
 console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
+//Invoking the interrupter function again:
 let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
-
-
-***********************************************************************/
-
-// your code here!
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
