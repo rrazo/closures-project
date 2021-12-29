a28f347bbe5aa30c70608c5984b9fe4ad0e45770
MatthewHautala <november12erika@gmail.com>
2021-12-29 14:56:52 -0700

    07

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -2,23 +2,25 @@
 Write a function named `funcTimer(time, func)` that will allow you to hand
 in a function and a number representing milliseconds. The `funcTimer` should use
 the global.setTimeout function to invoke the passed in function in `time` amount
-of seconds. 
+of seconds.
 
 There are no specs for this problem - try it in the console yourself to
 test your answer!
 
 Examples:
+setTimeout(() => {console.log("this is the first message")}, 5000);
+
+***********************************************************************/
+let funcTimer = function(time, func) {
+ setTimeout(func, time)
+}
+// your code here!
 
 function partyFunc () {
   console.log("Party time!")
 }
 
-funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
-
-
-***********************************************************************/
-
-// your code here!
+console.log(funcTimer(5000, partyFunc)); // in 5 seconds prints: "Party time!"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
