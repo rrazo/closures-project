372cc5986fc6b66220a7b05098d549ed201afd03
MatthewHautala <november12erika@gmail.com>
2021-12-29 14:14:05 -0700

    03

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -5,13 +5,16 @@ and sums them. Assign the below function to a variable using the const keyword.
 **Hint**: Use rest parameter syntax!
 
 Examples:
-arrowRestSum(3,5,6); // => 14
-arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
-arrowRestSum(0); // => 0
+
 ***********************************************************************/
+const arrowRestSum = (...nums) => {
+ let reducer = nums.reduce((sum, num) =>  (sum += num))
+return reducer
+}
 
-// your code here!
-
+arrowRestSum(3,5,6); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+arrowRestSum(0); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
